0f3fb9287528b6acb10bb24ea0c7945e2323bcd9
Evgeny Fomin <fominok@hotmail.com>
2023-11-15 21:04:49 +0100

    NUKE

--- src/app/version_upgrade.rs
+++
@@ -1,1 +0,0 @@
-//! Logic for version upgrade workflows.
--- src/mock_components.rs
+++
@@ -1,8 +0,0 @@
-//! Base (mock) components generic enough to be `MockComponent`, but missing
-//! from the standard library of Realm components.
-
-mod command_pallet;
-mod completing_input;
-
-pub(crate) use command_pallet::{CommandPallet, CommandPalletKey, KeyType};
-pub(crate) use completing_input::{key_event_to_cmd, CompletingInput, HistoryCompletionEngine};
