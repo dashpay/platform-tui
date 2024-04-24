15c8d5d7eb04944b6fbc2c0f7c122740f1e34bad
Ivan Shumkov <ivan@shumkov.ru>
2024-04-24 23:44:40 +0700

    refactor: introduce Rate

--- src/bin/query_test.rs
+++ src/bin/query_test.rs
@@ -20,6 +20,8 @@ use dapi_grpc::tonic::transport::Uri;
 use dapi_grpc::tonic::{Code, Status as TransportError};
 use dashmap::DashMap;
 use futures::future::join_all;
+use governor::clock::DefaultClock;
+use governor::state::{InMemoryState, NotKeyed};
 use governor::{Quota, RateLimiter};
 use rs_dapi_client::{
     Address, AddressList, DapiClient, DapiClientError, DapiRequest, RequestSettings,
@@ -29,21 +31,6 @@ use tokio::time::{interval, Instant};
 use tokio_util::sync::CancellationToken;
 use tracing::Instrument;
 use tracing_subscriber::EnvFilter;
-
-#[derive(clap::ValueEnum, Clone, Debug)]
-enum RateUnit {
-    Second,
-    Minute,
-}
-
-impl Display for RateUnit {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            RateUnit::Second => write!(f, "second"),
-            RateUnit::Minute => write!(f, "minute"),
-        }
-    }
-}
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -115,15 +102,15 @@ async fn main() {
         default_panic_hook(panic_info);
     }));
 
-    // Load configuration
     let config = Config::load();
+
+    let rate = Rate::new(args.rate, args.rate_unit);
 
     query_identities(
         &config,
         args.time.map(|t| Duration::from_secs(t.into())),
         args.connections,
-        args.rate,
-        args.rate_unit,
+        rate,
     )
     .await;
 }
@@ -132,11 +119,11 @@ async fn query_identities(
     config: &Config,
     duration: Option<Duration>,
     concurrent_connections: u16,
-    rate: u32,
-    rate_unit: RateUnit,
+    rate: Rate,
 ) {
     let start_time = Instant::now();
     let cancel_test = CancellationToken::new();
+    let rate = Arc::new(rate);
 
     let duration_message = if let Some(duration) = duration {
         format!(" for {} seconds", duration.as_secs_f32())
@@ -146,24 +133,11 @@ async fn query_identities(
 
     tracing::info!(
         "query {} per {} non existing identities with {} parallel connections{}",
-        rate,
-        rate_unit,
+        rate.rate,
+        rate.unit,
         concurrent_connections,
         duration_message
     );
-
-    // Rate limiter
-    let rate_limiter = {
-        let non_zero_rate = NonZeroU32::new(rate).expect("rate must be greater than zero");
-        let quota = match rate_unit {
-            RateUnit::Second => Quota::per_second(non_zero_rate),
-            RateUnit::Minute => Quota::per_minute(non_zero_rate),
-        };
-
-        let rate_limiter = RateLimiter::direct(quota);
-
-        Arc::new(rate_limiter)
-    };
 
     // Cancel the token after the duration
     if let Some(duration) = duration {
@@ -184,7 +158,7 @@ async fn query_identities(
     // Report summary every 10 seconds
     let report_task = {
         let cancel_report = cancel_test.clone();
-        let summary = Arc::clone(&summary);
+        let report_summary = Arc::clone(&summary);
 
         tokio::spawn(async move {
             let mut report_interval = interval(Duration::from_secs(10));
@@ -192,50 +166,36 @@ async fn query_identities(
             while !cancel_report.is_cancelled() {
                 report_interval.tick().await;
 
-                tracing::info!("{}", summary.report_message());
+                tracing::info!("{}", report_summary.report_message());
             }
         })
     };
 
     tasks.push(report_task);
 
-    let dapi_addresses: Vec<Address> = config
-        .dapi_addresses
-        .split(',')
-        .map(|uri| {
-            let uri = Uri::from_str(uri).expect("invalid uri");
-            Address::from(uri)
-        })
-        .collect();
+    let mut addresses = AddressPool::from(config.dapi_addresses.as_str());
 
     for connection_id in 0..concurrent_connections {
-        let rate_limiter = Arc::clone(&rate_limiter);
-        let cancel_task = cancel_test.clone();
-        let summary = Arc::clone(&summary);
-
         // Pick an address obe by one for each connection
-        let connection_address = dapi_addresses
-            .get(connection_id as usize % dapi_addresses.len())
-            .expect("address must be present");
+        // and create one client per connection
+        let client = DapiClient::new(addresses.next_one_address_list(), request_settings);
+        let connection_client = Arc::new(client);
 
-        let mut connection_address_list = AddressList::new();
-        connection_address_list.add(connection_address.clone());
-
-        // Create one client per connection
-        let client = DapiClient::new(connection_address_list, request_settings);
-        let client = Arc::new(client);
+        let connection_rate = Arc::clone(&rate);
+        let cancel_connection = cancel_test.clone();
+        let connection_summary = Arc::clone(&summary);
 
         // Send requests through the connection in a loop
-        let task = tokio::spawn(async move {
-            while !cancel_task.is_cancelled() {
+        let connection_task = tokio::spawn(async move {
+            while !cancel_connection.is_cancelled() {
                 // Wait for the rate limiter to allow further processing
                 tokio::select! {
-                    _ = rate_limiter.until_ready() => {},
-                    _ = cancel_task.cancelled() => return,
+                    _ = connection_rate.limiter.until_ready() => {},
+                    _ = cancel_connection.cancelled() => return,
                 }
 
-                let client = Arc::clone(&client);
-                let summary = Arc::clone(&summary);
+                let request_client = Arc::clone(&connection_client);
+                let request_summary = Arc::clone(&connection_summary);
 
                 // Send a request without waiting for the response,
                 // so we can send many requests in parallel through one connection
@@ -246,14 +206,14 @@ async fn query_identities(
                         connection_id = connection_id
                     );
 
-                    query_identity(client, request_settings, &summary)
+                    query_identity(request_client, request_settings, &request_summary)
                         .instrument(span)
                         .await
                 });
             }
         });
 
-        tasks.push(task);
+        tasks.push(connection_task);
     }
 
     join_all(tasks).await;
@@ -308,6 +268,45 @@ fn cancel_at(cancellation_token: CancellationToken, deadline: Instant) {
 
         cancellation_token.cancel()
     });
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum RateUnit {
+    Second,
+    Minute,
+}
+
+impl Display for RateUnit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RateUnit::Second => write!(f, "second"),
+            RateUnit::Minute => write!(f, "minute"),
+        }
+    }
+}
+
+struct Rate {
+    rate: u32,
+    unit: RateUnit,
+    limiter: RateLimiter<NotKeyed, InMemoryState, DefaultClock>,
+}
+
+impl Rate {
+    pub fn new(rate: u32, unit: RateUnit) -> Self {
+        let non_zero_rate = NonZeroU32::new(rate).expect("rate must be greater than zero");
+        let quota = match unit {
+            RateUnit::Second => Quota::per_second(non_zero_rate),
+            RateUnit::Minute => Quota::per_minute(non_zero_rate),
+        };
+
+        let limiter = RateLimiter::direct(quota);
+
+        Self {
+            rate,
+            unit,
+            limiter,
+        }
+    }
 }
 
 struct TestSummary {
@@ -377,5 +376,45 @@ impl TestSummary {
             "{elapsed_secs} secs passed. {total} processed ({rate} q/s): {oks} successful, {errors} failed{}",
             error_message
         )
+    }
+}
+
+struct AddressPool {
+    addresses: Vec<Address>,
+    current_index: usize,
+}
+
+impl AddressPool {
+    fn new(addresses: Vec<Address>) -> Self {
+        Self {
+            addresses,
+            current_index: 0,
+        }
+    }
+
+    fn next_address(&mut self) -> &Address {
+        let address = self.addresses.get(self.current_index).unwrap();
+        self.current_index = (self.current_index + 1) % self.addresses.len();
+        address
+    }
+
+    fn next_one_address_list(&mut self) -> AddressList {
+        let mut address_list = AddressList::new();
+        address_list.add(self.next_address().clone());
+        address_list
+    }
+}
+
+impl From<&str> for AddressPool {
+    fn from(addresses: &str) -> Self {
+        let addresses = addresses
+            .split(',')
+            .map(|uri| {
+                let uri = Uri::from_str(uri).expect("invalid uri");
+                Address::from(uri)
+            })
+            .collect();
+
+        Self::new(addresses)
     }
 }
